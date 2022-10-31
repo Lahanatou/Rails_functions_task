853609cbe8e5da15107c236fbf843ff11788e19d
lahanatou <nouryous30@gmail.com>
2022-10-31 14:41:55 +0100

    review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #devise_mailer.send("sample@gmail.com").deliver_later
+    devise_mailer.send("sample@gmail.com").deliver_later
     ContactMailer.contact("sample@gmail.com").deliver_later
 
   end
--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,36 +1,5 @@
-class UsersController < ApplicationController
-  before_action :correct_user, only: [:show]
-  skip_before_action :login_required, only: [:new, :create]
+require "rails_helper"
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in(@user)
-      redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
-    else
-      render :new
-    end
-  end
-
-  def show
-    @user = User.find(params[:id])
-    #devise_mailer.send("sample@gmail.com").deliver_later
-    ContactMailer.contact("sample@gmail.com").deliver_later
-
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image)
-  end
-
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to current_user unless current_user?(@user)
-  end
+RSpec.describe ContactMailer, type: :mailer do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
