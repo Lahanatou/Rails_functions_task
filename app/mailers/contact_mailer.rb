cef03167878f7f0bd6deaf51e801e8db15b63bf0
lahanatou <nouryous30@gmail.com>
2023-01-14 11:48:57 +0100

    commit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
     def contact(user)
         @user = user
-        mail(to: "sample@gmail.com", subject:"登録完了")
+        mail to: "sample@gmail.com", subject:"登録完了"
     end
-
+  
 end
