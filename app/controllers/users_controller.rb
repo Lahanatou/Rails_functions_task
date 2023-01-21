5e047997adf67bb53624082840b10a16487f6ffc
lahanatou <nouryous30@gmail.com>
2023-01-21 22:26:07 +0100

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      ContactMailer.with(to: @user.email, name: @user.name).welcome.deliver_later
       log_in(@user)
-      redirect_to user_path(@user.id), notice: 'Non spécifié Compte enregistré.'
+      redirect_to user_path(@user.id)
     else
       render :new
     end
-  end
+end
 
   def show
     @user = User.find(params[:id])
